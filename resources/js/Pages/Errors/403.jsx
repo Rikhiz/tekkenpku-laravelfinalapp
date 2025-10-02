7b1db51c0c0c7aa7509fd1657bfdf9d06ee18095
Rikhiz <fadelhizkil22@gmail.com>
2025-10-02 14:01:02 +0700

    Finall lagi??

--- resources/js/Pages/Errors/403.jsx
+++ resources/js/Pages/Errors/403.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Head, Link } from '@inertiajs/inertia-react';
+import { Head,Link } from "@inertiajs/react";
 import AppLayout from '@/Layouts/AppLayout';
 
 export default function Error403() {
